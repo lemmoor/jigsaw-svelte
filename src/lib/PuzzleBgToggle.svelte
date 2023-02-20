6623ccb4f6f8df605bbc33d802356360fb4df3b9
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-02-20 18:04:59 +0100

    fix checkbox always checked bug

--- src/lib/PuzzleBgToggle.svelte
+++ src/lib/PuzzleBgToggle.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { bg_alpha } from './stores';
 
+	let checked = $bg_alpha > 0;
+
 	function toggleBackground() {
 		if ($bg_alpha > 0) {
 			$bg_alpha = 0.0;
@@ -10,7 +12,7 @@
 	}
 </script>
 
-<label for="switch" class="unstyled mt-4 flex gap-2">
-	<input on:click={toggleBackground} type="checkbox" checked id="switch" class="checkbox" />
+<label for="switch" class="unstyled mt-4 flex cursor-pointer gap-2">
+	<input on:click={toggleBackground} type="checkbox" bind:checked id="switch" class="checkbox" />
 	<p>Background image</p>
 </label>
